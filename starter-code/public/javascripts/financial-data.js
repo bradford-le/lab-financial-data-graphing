897d29f73cdd74cea6f752f292f6cd3f8e5b76d6
Alex Buchan <alex.buchan@telefonica.net>
2017-07-12 18:15:05 +0200

    Iteration 2.3

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,15 +1,59 @@
-
 /*jshint esversion: 6*/
+
 function getBitCoinvalue() {
+  var data;
   $.ajax({
     url: "http://api.coindesk.com/v1/bpi/historical/close.json",
     method: "GET",
     success: function (response) {
-      console.log(response);
-      document.write(response);
+      data = JSON.parse(response).bpi;
+      console.log(data);
+      return data;
     },
     error: function (err) {
       console.log(err);
-    },
+    }
   });
+  console.log('End getBitCoinvalue function');
 }
+
+let x = getBitCoinvalue();
+console.log("XXXXXXXX!", x);
+
+var ctx = document.getElementById("myChart");
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: 'Bitcoin Price Index',
+            data: [1, 2, 3, 4, 5],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
