afc8c52b5022b8ab8c2c51ec68efb977e7bbead4
Alex Buchan <alex.buchan@telefonica.net>
2017-07-12 16:02:15 +0200

    My little pony

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,12 +1,15 @@
-function getBitCoinvalue(value) {
+/*jshint esversion: 6*/
+
+function getBitCoinvalue() {
   $.ajax({
-    url: "http://api.coindesk.com/v1/bpi/historical/close.json" + value,
+    url: "http://api.coindesk.com/v1/bpi/historical/close.json",
     method: "GET",
     success: function (response) {
       console.log(response);
+      document.write(response)
     },
     error: function (err) {
       console.log(err);
     },
-  })
+  });
 }
